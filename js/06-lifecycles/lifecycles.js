18ec64ef5e9ef75aafd16e1b753a8739e2a801e7
Tobias Madsen <tma@alex-andersen.dk>
2021-04-22 22:11:54 +0200

    some random lifecycle example

--- js/06-lifecycles/lifecycles.js
+++ js/06-lifecycles/lifecycles.js
@@ -5,9 +5,34 @@
 * Det kan være api kald, det kan være state det kan være promises.
 *
 2.
-* For
+* Før vi kan bruge Lifecycles skal vi bruge
 * */
 
 const Element  = (props) => {
 
+    const [state, setState] = React.useState(0)
+
+    React.useEffect(() => {
+        const getTodos = () => {
+            fetch('https://api.nasa.gov/planetary/apod?api_key=6c00onMJCcmkvRk3YILuFippHEStr8XC6f2sSRxc', {
+                method: "get",
+            }).then(data => {
+                return data.json()
+            }).then(res => setState(res))
+        }
+        getTodos()
+    }, [])
+
+    return (
+        <div className="container">
+            <h1>
+                {
+                    state.title
+                }
+            </h1>
+            <img src={state.url} alt={state.title} />
+        </div>
+    )
 }
+
+ReactDOM.render(<Element />, document.querySelector("#app"))
