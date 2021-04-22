d9b001501ceced6eee8d28cf5d39410cd963d34b
Tobias Madsen <tma@alex-andersen.dk>
2021-04-22 18:25:26 +0200

    changing branches

---
+++ js/06-lifecycles/lifecycles.js
@@ -0,0 +1,13 @@
+/*
+1.
+* Lifecycles er også det vi kalder sideeffects
+* dvs. at det er alt den data som kommer ude fra.
+* Det kan være api kald, det kan være state det kan være promises.
+*
+2.
+* For
+* */
+
+const Element  = (props) => {
+
+}
